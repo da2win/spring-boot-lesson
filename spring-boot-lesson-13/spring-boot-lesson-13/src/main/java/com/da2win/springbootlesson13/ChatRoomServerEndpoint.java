5f6821b6ff9bc7ad0012c1697fa8a04263e9d67c
Darwin <da2win@foxmail.com>
2018-10-17 14:49:06 +0800

    study: chat-room

--- spring-boot-lesson-13/spring-boot-lesson-13/src/main/java/com/da2win/springbootlesson13/ChatRoomServerEndpoint.java
+++ spring-boot-lesson-13/spring-boot-lesson-13/src/main/java/com/da2win/springbootlesson13/ChatRoomServerEndpoint.java
@@ -4,6 +4,8 @@ import javax.websocket.*;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * 聊天室
@@ -11,21 +13,33 @@ import java.io.IOException;
 @ServerEndpoint("/chat-room/{username}")
 public class ChatRoomServerEndpoint {
 
+    private static Map<String, Session> livingSessions = new ConcurrentHashMap<>();
+
     @OnOpen
     public void openSession(@PathParam("username") String username, Session session) {
-        RemoteEndpoint.Basic basicRemote = session.getBasicRemote();
-
-        sendText(session, "欢迎用户[" + username + "]来到聊天室");
+        String sessionId = session.getId();
+        livingSessions.put(sessionId, session);
+        sendTextAll("欢迎用户[" + username + "]来到聊天室");
     }
 
     @OnMessage
     public void onMessage(@PathParam("username") String username, Session session, String message) {
-        sendText(session, "用户[" + username + "] : " + message);
+//        sendText(session, "用户[" + username + "] : " + message);
+        sendTextAll("用户[" + username + "] : " + message);
+    }
+
+    private void sendTextAll(String message) {
+        livingSessions.forEach((sessionId, session)-> {
+            sendText(session, message);
+        });
     }
 
     @OnClose
     public void onClose(@PathParam("username") String username, Session session) {
-        // TODO
+        String sessionId = session.getId();
+        // 将当前的session移除, 并通知其他人当前用户已离开聊天室
+        livingSessions.remove(sessionId);
+        sendTextAll("用户["+username+"]已离开聊天室! ");
     }
 
     private void sendText(Session session, String message) {
--- spring-boot-lesson-13/spring-boot-lesson-13/src/main/java/com/da2win/springbootlesson13/SpringBootLesson13Application.java
+++ spring-boot-lesson-13/spring-boot-lesson-13/src/main/java/com/da2win/springbootlesson13/SpringBootLesson13Application.java
@@ -3,11 +3,13 @@ package com.da2win.springbootlesson13;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 import javax.websocket.server.ServerEndpoint;
 
 @SpringBootApplication
+@EnableWebSocket
 public class SpringBootLesson13Application {
 
 	public static void main(String[] args) {
@@ -17,5 +19,10 @@ public class SpringBootLesson13Application {
 	@Bean
 	public ServerEndpointExporter serverEndpointExporter() {
 		return new ServerEndpointExporter();
+	}
+
+	@Bean
+	public ChatRoomServerEndpoint chatRoomServerEndpoint() {
+		return new ChatRoomServerEndpoint();
 	}
 }
